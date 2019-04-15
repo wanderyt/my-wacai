4267497bec917fc913edfa1f7ff50dc15993b77f
wanderyt <wanderyt1988@163.com>
2019-04-15 18:28:08 +0800

    Page details of fin item.

---
+++ src/modules/fin-item-details/config.js
@@ -0,0 +1,19 @@
+export const comCatItems = [{
+  category: '交通',
+  subcategory: '打车'
+}, {
+  category: '生活',
+  subcategory: '水电煤气'
+}, {
+  category: '骐骐',
+  subcategory: '玩具'
+}, {
+  category: '生活',
+  subcategory: '生活用品'
+}, {
+  category: '周末',
+  subcategory: '晚餐'
+}, {
+  category: '周末',
+  subcategory: '午餐'
+}];
