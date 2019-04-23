b40f164fad20bc222598ada7f39e2bb460f2fa7c
wanderyt <wanderyt1988@163.com>
2019-04-23 22:08:45 +0800

    Add fin list related api endpoint.

---
+++ server/router/getHistoryExpense.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const log4js = require('log4js');
+const logger = log4js.getLogger('wacai');
+const {createDBConnection, closeDB, getMonthlyTotal} = require('../db/dao');
+
+router.get('/getHistoryExpense', (req, res) => {
+  logger.info('api /getHistoryExpense');
+  let db = createDBConnection();
+  let getMonthlyTotalPromise = getMonthlyTotal(db);
+  getMonthlyTotalPromise
+    .then((data) => {
+      closeDB(db);
+      if (data.err) {
+        logger.error('api /getHistoryExpense failed with error');
+        logger.error(data.err);
+        res.statusCode = 500;
+        res.send({
+          status: false,
+          error: data.err
+        });
+      } else {
+        logger.info('api /getHistoryExpense success');
+        res.statusCode = 200;
+        res.send({
+          status: true,
+          data: formatHistoryExpense(data.rows)
+        });
+      }
+    })
+});
+
+const formatHistoryExpense = (data = []) => {
+  let highestMonthAmount = 0;
+  let historyExpense = data.map((item) => {
+    if (highestMonthAmount < item.total) {
+      highestMonthAmount = item.total;
+    }
+
+    return {
+      year: item.year_month.substr(0, 4),
+      month: item.year_month.substr(5, 2),
+      amount: item.total
+    };
+  });
+
+  return {
+    highestMonthAmount,
+    items: historyExpense
+  };
+}
+
+module.exports = {
+  router
+};
