1f7f1f94037e7458b31a098334e857c3292f6682
wanderyt <wanderyt1988@163.com>
2019-05-07 15:57:37 +0800

    Adopt latest header toolbar change.

---
+++ src/modules/search-fin-item/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBox from '../search-box';
+
+const SearchFinItem = () => {
+  return (
+    <div className='SearchFinItem'>
+      Search fin item
+    </div>
+  );
+};
+
+export default SearchFinItem;
