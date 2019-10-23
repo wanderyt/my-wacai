6d0581aca7677a5308bb03da995c2d16aad7b55b
David, Ren <yuren@ebay.com>
2019-10-23 12:42:59 +0800

    Add build sources temporarily to support cloud.

---
+++ build/precache-manifest.66e2e682a1a1be9813878eadd4fe4e39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b579ad4c2255d3047bcabdf7737f32",
+    "url": "/static/media/search.35b579ad.svg"
+  },
+  {
+    "revision": "8846e8d04b08c1c7b2a36776ea29b095",
+    "url": "/static/media/fin-l.8846e8d0.jpg"
+  },
+  {
+    "revision": "373e500570593ad15aca1151c8c249e3",
+    "url": "/static/media/arrow.373e5005.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3d2f5f7c2efc12bcff5a",
+    "url": "/static/js/main.05dda752.chunk.js"
+  },
+  {
+    "revision": "c5a6cfb8638a49241cab",
+    "url": "/static/js/2.014264b8.chunk.js"
+  },
+  {
+    "revision": "3d2f5f7c2efc12bcff5a",
+    "url": "/static/css/main.3ce14354.chunk.css"
+  },
+  {
+    "revision": "c5a6cfb8638a49241cab",
+    "url": "/static/css/2.78c4eeee.chunk.css"
+  },
+  {
+    "revision": "e2d4b3f218b580d3fbaff69320368dd8",
+    "url": "/index.html"
+  }
+];
