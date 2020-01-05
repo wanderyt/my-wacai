bd429b5418a777a085b17e3e11d56eaec6485771
David, Ren <yuren@ebay.com>
2020-01-05 20:46:44 +0800

    Add build resources.

--- build/precache-manifest.322f0ad2fa9079e81367798d83d1e9d0.js
+++ build/precache-manifest.322f0ad2fa9079e81367798d83d1e9d0.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/static/media/arrow.373e5005.svg"
   },
   {
-    "revision": "734fd2a40a524c78fc51",
-    "url": "/static/css/main.1517b3c5.chunk.css"
+    "revision": "c167493d29256aff71d7",
+    "url": "/static/css/main.d84159e0.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "f6e67539e25adbf860808313c8e75ce5",
-    "url": "/static/media/digital-7(mono+italic).f6e67539.ttf"
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.1e670d88.ttf"
   },
   {
     "revision": "c5a6cfb8638a49241cab",
@@ -28,31 +28,31 @@ self.__precacheManifest = [
     "url": "/static/media/geo-icon.c8d3ba55.svg"
   },
   {
-    "revision": "734fd2a40a524c78fc51",
-    "url": "/static/js/main.fbf6a1a4.chunk.js"
+    "revision": "c167493d29256aff71d7",
+    "url": "/static/js/main.a85a2f9c.chunk.js"
   },
   {
     "revision": "8846e8d04b08c1c7b2a36776ea29b095",
     "url": "/static/media/fin-l.8846e8d0.jpg"
   },
   {
+    "revision": "7c6e146fe91c1b2633316a6aee97a9af",
+    "url": "/static/media/digital-7(italic).7c6e146f.ttf"
+  },
+  {
     "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
     "url": "/static/media/digital-7(mono).58045dab.ttf"
   },
   {
-    "revision": "1e670d88b23c7ab956f1829e3828a210",
-    "url": "/static/media/digital-7.1e670d88.ttf"
-  },
-  {
-    "revision": "7c6e146fe91c1b2633316a6aee97a9af",
-    "url": "/static/media/digital-7(italic).7c6e146f.ttf"
+    "revision": "f6e67539e25adbf860808313c8e75ce5",
+    "url": "/static/media/digital-7(mono+italic).f6e67539.ttf"
   },
   {
     "revision": "c5a6cfb8638a49241cab",
     "url": "/static/css/2.78c4eeee.chunk.css"
   },
   {
-    "revision": "cbe35128d660cb9357633a525552469a",
+    "revision": "df6e756ca5da69a0d7c8003ea0749609",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a804ed1c5f6e035d931e758bd49f20a.js"
+  "/precache-manifest.322f0ad2fa9079e81367798d83d1e9d0.js"
 );
 
 workbox.clientsClaim();
