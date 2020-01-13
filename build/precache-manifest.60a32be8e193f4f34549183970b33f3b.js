e48b0f1f59e1f3e38841e209a8c71c901b99821c
David, Ren <yuren@ebay.com>
2020-01-13 16:03:04 +0800

    Add build resources.

--- build/precache-manifest.60a32be8e193f4f34549183970b33f3b.js
+++ build/precache-manifest.60a32be8e193f4f34549183970b33f3b.js
@@ -4,20 +4,20 @@ self.__precacheManifest = [
     "url": "/static/media/geo-icon.c8d3ba55.svg"
   },
   {
-    "revision": "355cc957f64c2ba417ac",
-    "url": "/static/css/main.d84159e0.chunk.css"
+    "revision": "e9a964d54fc4c41f9541",
+    "url": "/static/css/main.efff3b11.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
-    "url": "/static/media/digital-7(mono).58045dab.ttf"
+    "revision": "7c6e146fe91c1b2633316a6aee97a9af",
+    "url": "/static/media/digital-7(italic).7c6e146f.ttf"
   },
   {
-    "revision": "24f0017354f28e216c52",
-    "url": "/static/js/2.2641a3d1.chunk.js"
+    "revision": "b630a7f2ae3f81e21058",
+    "url": "/static/js/2.089f11bc.chunk.js"
   },
   {
     "revision": "373e500570593ad15aca1151c8c249e3",
@@ -28,31 +28,31 @@ self.__precacheManifest = [
     "url": "/static/media/search.35b579ad.svg"
   },
   {
-    "revision": "355cc957f64c2ba417ac",
-    "url": "/static/js/main.a5a0b430.chunk.js"
+    "revision": "e9a964d54fc4c41f9541",
+    "url": "/static/js/main.f26ebcf1.chunk.js"
   },
   {
     "revision": "8846e8d04b08c1c7b2a36776ea29b095",
     "url": "/static/media/fin-l.8846e8d0.jpg"
   },
   {
-    "revision": "7c6e146fe91c1b2633316a6aee97a9af",
-    "url": "/static/media/digital-7(italic).7c6e146f.ttf"
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.1e670d88.ttf"
   },
   {
     "revision": "f6e67539e25adbf860808313c8e75ce5",
     "url": "/static/media/digital-7(mono+italic).f6e67539.ttf"
   },
   {
-    "revision": "1e670d88b23c7ab956f1829e3828a210",
-    "url": "/static/media/digital-7.1e670d88.ttf"
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/static/media/digital-7(mono).58045dab.ttf"
   },
   {
-    "revision": "24f0017354f28e216c52",
+    "revision": "b630a7f2ae3f81e21058",
     "url": "/static/css/2.78c4eeee.chunk.css"
   },
   {
-    "revision": "d145faae5bdb1afb3db94559407dc971",
+    "revision": "f60b5c3423a17577389e12512b88c05f",
     "url": "/index.html"
   }
 ];
