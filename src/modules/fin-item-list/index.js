f45e8785267bc99843931dd922f7495e3fd2c8b4
David, Ren <yuren@ebay.com>
2020-01-20 17:41:09 +0800

    Add button group for fin item list panel.

--- src/modules/fin-item-list/index.js
+++ src/modules/fin-item-list/index.js
@@ -1,19 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {connect} from 'react-redux';
 import Axios from 'axios';
 import MonthExpense from './month-expense';
 import HeaderToolbar from '../header-toolbar';
+import BottomButtonGroup from '../buttom-button-group';
 
 // import historyExpense from './mockData.json';
 
 const FinItemList = ({dispatch}) => {
   const [historyExpense, setHistoryExpense] = useState({});
+  const [btnExpanded, setBtnExpanded] = useState(false);
+  const headerEl = useRef(null);
 
   useEffect(() => {
     let now = new Date();
     Axios.get(`/api/wacai/getHistoryExpense?year=${now.getFullYear()}&month=${now.getMonth() + 1}`)
       .then(({data}) => {
         let responseData = data.data || {};
+        setBtnExpanded(false);
         setHistoryExpense(responseData);
       }, ({response}) => {
         if (response.status === 401) {
@@ -24,8 +28,11 @@ const FinItemList = ({dispatch}) => {
       });
   }, []);
 
+  useEffect(() => {
+  }, [headerEl]);
+
   return (
-    <div className='FinItemList'>
+    <div className='FinItemList' ref={headerEl}>
       <HeaderToolbar />
       {
         historyExpense.items && historyExpense.items.map((monthItem, index) => (
@@ -37,6 +44,9 @@ const FinItemList = ({dispatch}) => {
               barWidthRatio={monthItem.amount / historyExpense.highestMonthAmount} />
           </div>
         ))
+      }
+      {
+        historyExpense.items && <BottomButtonGroup isExpanded={btnExpanded} scrollIntoViewRef={headerEl} />
       }
     </div>
   );
