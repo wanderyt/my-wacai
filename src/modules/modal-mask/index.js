fececc85e8cc48506ef783df650c87832ebb5af6
David Ren <yuren@stubhub.com>
2020-04-07 10:47:25 +0800

    Add modal mask component

---
+++ src/modules/modal-mask/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+/**
+ * A component that shows a modal mask over the entire screen. It uses RGBA as background color
+ * so opacity is achieved without using the CSS opacity property, so that children are not affected. We
+ * provide a default z-index of 9999, but that can be customized. All children provided will
+ * be added as children of the component.
+ *
+ * Developers can modify this component to expose more things to be customized as needed.
+ */
+
+class ModalMask extends React.Component {
+  onClick = () => {
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  }
+
+  render() {
+    let modalMaskCls = 'ModalMask__container';
+
+    // Add any extra CSS class
+    if (this.props.extraCls) {
+      modalMaskCls += ' ' + this.props.extraCls;
+    }
+
+    return <div className={modalMaskCls} onClick={this.onClick}>{this.props.children}</div>;
+  }
+}
+
+ModalMask.propTypes = {
+  extraCls: PropTypes.string,
+  onClick: PropTypes.func
+};
+
+ModalMask.defaultProps = {
+  extraCls: ''
+};
+
+export default ModalMask;
