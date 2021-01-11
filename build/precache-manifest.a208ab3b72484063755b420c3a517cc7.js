4c6f3995d36e1bc482d46cb3566d236006ef55d6
Wanderyt <wanderyt1988@163.com>
2021-01-11 13:55:33 +0800

    Add build resources.

---
+++ build/precache-manifest.a208ab3b72484063755b420c3a517cc7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "05dbc954aa55e5796a66a261563c338d",
+    "url": "/static/media/geo-icon.05dbc954.svg"
+  },
+  {
+    "revision": "942354d1f67c4cc71687",
+    "url": "/static/css/main.0ec489b6.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/static/media/digital-7(mono).58045dab.ttf"
+  },
+  {
+    "revision": "20a3b011e8ee61936007",
+    "url": "/static/js/2.cba811ca.chunk.js"
+  },
+  {
+    "revision": "5ccd269a38e9d8bfd48e6dcb401d9e5f",
+    "url": "/static/media/arrow.5ccd269a.svg"
+  },
+  {
+    "revision": "fc20e1ef690617da7d8de03ad4aee792",
+    "url": "/static/media/search.fc20e1ef.svg"
+  },
+  {
+    "revision": "942354d1f67c4cc71687",
+    "url": "/static/js/main.2a20d45f.chunk.js"
+  },
+  {
+    "revision": "6e25df312439e6fe28ccae8b4f5bac34",
+    "url": "/static/media/details-white.6e25df31.svg"
+  },
+  {
+    "revision": "8846e8d04b08c1c7b2a36776ea29b095",
+    "url": "/static/media/fin-l.8846e8d0.jpg"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "7c6e146fe91c1b2633316a6aee97a9af",
+    "url": "/static/media/digital-7(italic).7c6e146f.ttf"
+  },
+  {
+    "revision": "f6e67539e25adbf860808313c8e75ce5",
+    "url": "/static/media/digital-7(mono+italic).f6e67539.ttf"
+  },
+  {
+    "revision": "20a3b011e8ee61936007",
+    "url": "/static/css/2.c08f39fa.chunk.css"
+  },
+  {
+    "revision": "22044c63cc5a9fee7f0e71334cc487e4",
+    "url": "/index.html"
+  }
+];
