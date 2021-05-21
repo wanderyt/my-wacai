c8ec1cef1456ab2580ecff13878f66dc26d725b9
wanderyt <wanderyt1988@163.com>
2021-05-21 16:38:33 +0800

    Set checkbox to be checked by default for comment hint dialog.

--- src/modules/checkbox/index.tsx
+++ src/modules/checkbox/index.tsx
@@ -1,18 +1,26 @@
-import React, {ChangeEvent, FC} from 'react';
+import React, {ChangeEvent, FC, useEffect, useState} from 'react';
 import './index.scss';
 
 interface Props {
   disabled?: boolean;
   onChange?: (evt: ChangeEvent) => void;
+  defaultChecked?: boolean;
   className?: string
 };
 
-const Checkbox: FC<Props> = ({disabled, onChange, className = ''}) => {
+const Checkbox: FC<Props> = ({disabled, onChange, className = '', defaultChecked = false}) => {
+  const [checked, setChecked] = useState<boolean>(defaultChecked);
+
+  useEffect(() => {
+    setChecked(defaultChecked);
+  }, [defaultChecked])
+
   return (
     <input
       type="checkbox"
       disabled={disabled}
       onChange={onChange}
+      checked={checked}
       className={className}
     />
   );
